0962a60e835616d21dbb01ebf699d6e8e6f3f762
Gustavo Souza <ss.gustavo2007@gmail.com>
2025-06-18 14:10:40 -0300

    Mexendo com a PokeApi

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,25 @@
-const offset = 0;
-const limit = 20;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon ${pokemon.type}">
+     <span class="number">#${pokemon.number}</span>
+     <span class="name">${pokemon.name}</span>
+  
+      <div class="detail">
+          <ol class="types">
+            ${pokemon.types
+              .map((type) => `<li class="type">${type}</li>`)
+              .join("")}
+          </ol>
+          <img src="${pokemon.photo}"
+              alt="${pokemon.name}"/>
+      </div>
+    </li>
+    `;
+}
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((jsonBody) => {
-    debugger;
-    console.log(jsonBody);
-  })
-  .catch((error) => console.error(Error));
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getPokemon().then((pokemons = []) => {
+  const newHTML = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHTML;
+});
